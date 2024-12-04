1f6836410e5b20b4650a9d81ddc2b358266ab5d8
Podlipenko Mariia <podlipenko@gmail.com>
2024-12-05 00:09:05 +0200

    Update app info

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
